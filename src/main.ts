aafbbf8a681f4879592f311869ebd1d30fd6b49b
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2024-04-20 12:14:22 +0200

    Removing Modbus Stuff again.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { FreeAtHome } from '@busch-jaeger/free-at-home';
-import fetch from 'cross-fetch';
+
 
 const freeAtHome = new FreeAtHome();
 freeAtHome.activateSignalHandling();
 
 var url: string = "http://192.168.0.10/cm?cmnd=status%208";
+
 
 // Regular Meter
 // {"StatusSNS":{"Time":"2023-09-10T14:43:28","Regular":{"Total_in":2869.4216,"Power_curr":262}}}
@@ -21,81 +22,146 @@ interface TasmotaResponseHeatPumpConsumption { Verbrauch_T1: number, Verbrauch_T
 
 // Basic Guard Function
 const isTasmotaResponseRegular = (data: any): data is TasmotaResponseRegularResponse => {
-  return data.StatusSNS.Time == 'date' && data.StatusSNS.Regular.Total_in == 'number' && data.StatusSNS.Regular.Power_curr == 'number'
+  return  true // data.StatusSNS.Time == 'date' && data.StatusSNS.Regular.Total_in == 'number' && data.StatusSNS.Regular.Power_curr == 'long'
 }
 
+
+
 const isTasmotaResponseHeatPump = (data: any): data is TasmotaResponseHeatPumpResponse => {
-  return data.StatusSNS.Time == 'date' && data.StatusSNS.Heating.Verbrauch_T1 == 'number' && data.StatusSNS.Heating.Verbrauch_T2 == 'number'
+  return data.StatusSNS.Time == 'date' && data.StatusSNS.Heating.Verbrauch_T1 == 'long' && data.StatusSNS.Heating.Verbrauch_T2 == 'long'
 }
+
+main();
 
 async function main() {
 
   console.log("Main triggered");
 
-  const energyMeter = await freeAtHome.createEnergyMeterDevice("TasmotaEnergyMeter", "Tasmota Regular Meter")
+  // triggering every 5 seconds
+  var minutes = 5, the_interval = minutes * 1000 // * 60 ;
+
+  /*
+  if (url == '') {
+    console.warn('URL is not set. Please enter your URL in the settings');
+    return;
+  }
+  */
+
+  // open connection to a tcp line
+/*
+  try {
+    client.connectTCP("192.168.70.37", { port: 502 });
+    client.setID(1);
+  } catch (error) {
+    console.error(error);
+  }
+*/
+
+
+  /*
+  // Wasser Zähler
+  const waterMeter = await freeAtHome.createWaterMeterDevice("DemoWaterMeter", "Demo Water Meter")
+  await new Promise(f => setTimeout(f, 1000));
+  waterMeter.setAutoKeepAlive(true);
+  waterMeter.setAutoConfirm(true);
+  waterMeter.isAutoConfirm = true;
+
+  console.log("waterMeter Created");
+  setInterval(async function () {
+  await fetchRegularConsumption().then(function (tasmotaData) {  
+    waterMeter.setWaterConsumed("1111");
+    waterMeter.setWaterConsumed("2222");
+    console.log("Update Data.")
+
+  }).catch(function (err) {
+    console.log('Error on updating values of Tasmota reader', err);
+  });
+
+  }, the_interval);
+  */
+
+/*
+  
+  // this on is creating a time out. 
+  const energyMeter = await freeAtHome.createEnergyMeterDevice("TasmotaEnergyMeter", "Tasmota Regular Meter");
+  console.log("EnergyMeterDevice Created");
+  energyMeter.setMaxListeners(10)
+  await new Promise(f => setTimeout(f, 1000));
+  energyMeter.setAutoKeepAlive(true);
+  energyMeter.setAutoConfirm(true);
+  energyMeter.isAutoConfirm = true;
+  energyMeter.setHomePowerConsumption("10")
+  energyMeter.setAutoConfirm(true);
+  energyMeter.setAutoKeepAlive(true)
+
+      // Use random static data.
+      energyMeter.setSelfConsumption("20")
+      energyMeter.setPowerToGrid("20")
+      energyMeter.setHomePowerConsumption("20")
+*/
+
+/*
+  await fetchRegularConsumption().then(function (tasmotaData) {
+    // do something with the data.
+    let total = tasmotaData.StatusSNS.Regular.Total_in;
+    let current = tasmotaData.StatusSNS.Regular.Power_curr;
+
+    // Use random static data.
+    energyMeter.setSelfConsumption("20")
+    energyMeter.setPowerToGrid("20")
+    energyMeter.setHomePowerConsumption("20")
+
+    console.log("Update Data.")
+
+  }).catch(function (err) {
+    console.log('Error on updating values of Tasmota reader', err);
+  });
+
+  }, the_interval);
+
+  */
+
+
+  //this is not show in the UI. 
+  const energyMeter = await freeAtHome.createEnergyOneWayMeterV2Device("TasmotaEnergyMeter", "Tasmota Regular Meter")
+  energyMeter.setAutoKeepAlive(true);
+  energyMeter.setAutoConfirm(true);
+  energyMeter.isAutoConfirm = true;
+  energyMeter.setCurrentPowerConsumed("10");
+  energyMeter.setAutoKeepAlive(true);
+  energyMeter.setAutoConfirm(true);
+
+  console.log("Device Created");
+
+
+
+  setInterval(async function () {
+
+    // Use random static data.
+    energyMeter.setCurrentPowerConsumed("1111");
+    energyMeter.setTotalEnergyExported("2222");
+    energyMeter.setExportedEnergyCostsToday("33333");
+    energyMeter.setExportedEnergyCostsTotal("4444");
+    
+    console.log("Update Data.")
+
+  }, the_interval);
+  
+
+
+  /*
+
+  const energyMeter = await freeAtHome.createEnergyOneWayMeterV2Device("TasmotaEnergyMeter", "Tasmota Regular Meter")
   energyMeter.setAutoKeepAlive(true);
   energyMeter.setAutoConfirm(true);
   energyMeter.isAutoConfirm = true;
 
+  energyMeter.setCurrentPowerConsumed("1111");
+  energyMeter.setTotalEnergyExported("2222");
+  energyMeter.setExportedEnergyCostsToday("33333");
+  energyMeter.setExportedEnergyCostsTotal("3333");
+  */
 
-  // we have 500 queries per day, so running ever 3 minutes
-  var minutes = 3, the_interval = minutes * 1000;
-  setInterval(async function () {
+} // main
 
-    if (url == '') {
-      console.warn('URL is not set. Please enter your URL in the settings');
-      return;
-    }
 
-    console.log("Update Data.")
-    await fetchRegularConsumption().then(function (tasmotaData) {
-      // do something with the data.
-      let total = tasmotaData.StatusSNS.Regular.Total_in;
-      let current = tasmotaData.StatusSNS.Regular.Power_curr;
-
-      energyMeter.setHomePowerConsumption(current + '');
-      energyMeter.setSelfConsumption(total + '');
-
-    }).catch(function (err) {
-      console.log('Error on updating values of Tasmoate readers station', err);
-    });
-  }, the_interval);
-}
-
-/**
- * fetchAccessToken fetches the OAuth2 token from meteomatics for getting the weather data in a 2nd step
- * The Token is value for 2 h, we we pull one for every call. 
- * @returns OAuth2 Token as a String
- */
-async function fetchRegularConsumption() {
-  const resp = await fetch(url, {
-    method: 'GET'
-  });
-  const jsonResponse = await resp.json();
-  // Use Type Guard.
-  if (isTasmotaResponseRegular(jsonResponse)) {
-    return jsonResponse;
-  } else if (isTasmotaResponseHeatPump(jsonResponse)) {
-    return jsonResponse;
-  } else {
-    throw Error('Parsed string is not an Tasmota Reponse.');
-  }
-}
-
-main();
-
-// Get notified about changes in the configuration of the add on
-//#################################################################################
-
-import { AddOn } from '@busch-jaeger/free-at-home';
-
-const metaData = AddOn.readMetaData();
-
-const addOn = new AddOn.AddOn(metaData.id);
-
-addOn.on("configurationChanged", (configuration: AddOn.Configuration) => {
-  // TODO: Remove, as this would also log out the password.
-  console.log(configuration);
-  url = configuration.default?.items?.["URL"] ?? "";
-})
-addOn.connectToConfiguration();
